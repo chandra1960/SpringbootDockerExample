110c8e7769a7a550a37eb61da3f10f42c6f9d6ee
Chandra1960 <chandra.srdev@gmail.com>
2018-10-15 17:54:02 -0500

    first commit

---
+++ src/main/java/com/dockerexample/springbootdockerexample/SpringbootdockerexampleApplication.java
@@ -0,0 +1,12 @@
+package com.dockerexample.springbootdockerexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdockerexampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootdockerexampleApplication.class, args);
+    }
+}
---
+++ src/test/java/com/dockerexample/springbootdockerexample/SpringbootdockerexampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dockerexample.springbootdockerexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootdockerexampleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
